9e7aca5f941e308f6aebce4044a9b777f64a91fe
Bhanuka94 <thebenuka@gmail.com>
2020-07-21 07:05:21 +0530

    Updates

---
+++ precache-manifest.a4d307c4119e45dc258fc826741afee6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd654899d41fd6d81d12189b68dc5329",
+    "url": "/covid19_tracker/index.html"
+  },
+  {
+    "revision": "d9a423917507504fec66",
+    "url": "/covid19_tracker/static/css/main.a55a5851.chunk.css"
+  },
+  {
+    "revision": "365d84c130d9f6e2dd81",
+    "url": "/covid19_tracker/static/js/2.eee71a07.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19_tracker/static/js/2.eee71a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9a423917507504fec66",
+    "url": "/covid19_tracker/static/js/main.264eec93.chunk.js"
+  },
+  {
+    "revision": "8447e5eac2318c5f0b43",
+    "url": "/covid19_tracker/static/js/runtime-main.af0f8073.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19_tracker/static/media/image.d7265326.png"
+  }
+]);
